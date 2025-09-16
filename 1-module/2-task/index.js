8752796e30db47b719016502eb4ed0601925b749
mefremov <mefremov@bescargo.ru>
2025-09-16 10:41:38 +0300

    add: added solution 2-task in 1 module

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -1,24 +1,25 @@
-/**
- * Эту функцию трогать не нужно
- */
 function print(text) {
   console.log(text);
 }
 
-/**
- * Эту функцию нужно поменять так,
- * чтобы функция sayHello работала корректно
- */
 function isValid(name) {
-  // ваш код...
+  if (name === null) {
+    return false;
+  } else if (name.length < 4) {
+    return false;
+  } else if (name.includes(" ")) {
+    return false;
+  } else {
+    return true;
+  }
 }
 
 function sayHello() {
-  let userName = prompt('Введите ваше имя');
+  let userName = prompt("Введите ваше имя");
 
   if (isValid(userName)) {
     print(`Welcome back, ${userName}!`);
   } else {
-    print('Некорректное имя');
+    print("Некорректное имя");
   }
 }
