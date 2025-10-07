bbb56af6ac79802b6ac090f3c182e2876b0d9661
mefremov <mefremov@bescargo.ru>
2025-10-07 10:18:41 +0300

    add: added solution 3-task in 4 module

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,26 @@
 function highlight(table) {
-  // ваш код...
+  const rows = table.querySelectorAll("tbody tr");
+  rows.forEach((row) => {
+    const cells = row.querySelectorAll("td");
+
+    const statusCell = cells[3];
+    if (statusCell.hasAttribute("data-available")) {
+      const isAvailable = statusCell.getAttribute("data-available") === "true";
+      row.classList.add(isAvailable ? "available" : "unavailable");
+    } else {
+      row.setAttribute("hidden", true);
+    }
+
+    const genderCell = cells[2];
+    if (genderCell.textContent.trim() === "m") {
+      row.classList.add("male");
+    } else if (genderCell.textContent.trim() === "f") {
+      row.classList.add("female");
+    }
+
+    const ageCell = cells[1];
+    if (parseInt(ageCell.textContent) < 18) {
+      row.style.textDecoration = "line-through";
+    }
+  });
 }
