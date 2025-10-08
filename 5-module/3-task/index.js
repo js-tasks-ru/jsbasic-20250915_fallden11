c23b2d9ef12e281533e8686e31e26cd9fc186cfe
mefremov <mefremov@bescargo.ru>
2025-10-08 17:03:08 +0300

    added solution 3-task in 5 module

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,37 @@
 function initCarousel() {
-  // ваш код...
+  const carousel = document.querySelector(".carousel");
+  const carouselInner = carousel.querySelector(".carousel__inner");
+  const arrowLeft = carousel.querySelector(".carousel__arrow_left");
+  const arrowRight = carousel.querySelector(".carousel__arrow_right");
+  const slideWidth = carouselInner.offsetWidth;
+  let currentSlide = 0;
+  const totalSlides = 4;
+
+  arrowLeft.style.display = "none";
+
+  arrowRight.addEventListener("click", () => {
+    currentSlide++;
+    updateCarousel();
+  });
+
+  arrowLeft.addEventListener("click", () => {
+    currentSlide--;
+    updateCarousel();
+  });
+
+  function updateCarousel() {
+    const offset = -slideWidth * currentSlide;
+    carouselInner.style.transform = `translateX(${offset}px)`;
+    if (currentSlide === 0) {
+      arrowLeft.style.display = "none";
+    } else {
+      arrowLeft.style.display = "";
+    }
+
+    if (currentSlide === totalSlides - 1) {
+      arrowRight.style.display = "none";
+    } else {
+      arrowRight.style.display = "";
+    }
+  }
 }
