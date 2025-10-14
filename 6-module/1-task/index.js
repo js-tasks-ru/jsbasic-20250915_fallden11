f8d0066fe320ef6de9235909017c1a4c7ec2fe52
mefremov <mefremov@bescargo.ru>
2025-10-14 13:24:37 +0300

    added solution 1-task in 6 module

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,46 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.elem = this.#createTable();
+  }
+
+  #createTable() {
+    const table = document.createElement("table");
+    const thead = document.createElement("thead");
+
+    thead.innerHTML = `
+    <tr>
+        <th>Имя</th>
+        <th>Возраст</th>
+        <th>Зарплата</th>
+        <th>Город</th>
+        <th></th>
+      </tr>
+    `;
+
+    table.appendChild(thead);
+    const tbody = document.createElement("tbody");
+
+    for (let row of this.rows) {
+      const tr = document.createElement("tr");
+
+      tr.innerHTML = `
+        <td>${row.name}</td>
+        <td>${row.age}</td>
+        <td>${row.salary}</td>
+        <td>${row.city}</td>
+        <td><button>X</button></td>
+      `;
+
+      tr.querySelector("button").addEventListener("click", () => {
+        tr.remove();
+      });
+
+      tbody.appendChild(tr);
+    }
+
+    table.appendChild(tbody);
+    return table;
   }
 }
